501a499ce9e78b860a665d8ab212955eafb067d6
K_STYER <112352977+KSTYER1@users.noreply.github.com>
2026-05-01 10:18:56 +0200

    Add src/plugin-support.h

---
+++ src/plugin-support.h
@@ -0,0 +1,22 @@
+/* src/plugin-support.h */
+#pragma once
+
+#include <obs-module.h>
+
+#ifndef PLUGIN_NAME
+#define PLUGIN_NAME "obs-scene-organiser"
+#endif
+
+#ifndef PLUGIN_VERSION
+#define PLUGIN_VERSION "1.0.0"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void obs_log(int log_level, const char *format, ...);
+
+#ifdef __cplusplus
+}
+#endif
